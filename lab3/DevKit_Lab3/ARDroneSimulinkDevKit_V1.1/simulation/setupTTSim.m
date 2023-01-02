9c51a2291fd9b4ae99b5e1ab73019c06ddbf551c
RafaelaChaffilla <80000978+RafaelaChaffilla@users.noreply.github.com>
2023-01-02 18:36:18 +0000

    save images automático

--- lab3/DevKit_Lab3/ARDroneSimulinkDevKit_V1.1/simulation/setupTTSim.m
+++ lab3/DevKit_Lab3/ARDroneSimulinkDevKit_V1.1/simulation/setupTTSim.m
@@ -8,7 +8,11 @@
 %  exactly the same for both simulation and real-time Wi-Fi control.
 %  
 %  =======================================================================
-
+%   Modified by Group 2:
+%
+%   Duarte Lopes,      95783
+%   José Medeiro,      95811
+%   Rafaela Chaffilla, 95840
 %%
 %  Cleaning workspace
 bdclose all;
@@ -62,22 +66,23 @@ switch choice
         end
 
         % Plot altitude tracking for different k_w
-        figure();
+        fig1 = figure();
         hold on;
         plot(sim_results(1).pd.time, sim_results(1).pd.signals.values(3, :));
         for c = 1:size(sim_results,2)
             plot(sim_results(c).tout, - sim_results(c).h_sim.signals.values);
         end
         xlim([0 30]);
-        legend('desired', sprintf('k = %0.1f', k_w_vector(1)), ...
-            sprintf('k = %0.1f', k_w_vector(2)), ...
-            sprintf('k = %0.1f', k_w_vector(3)), ...
-            sprintf('k = %0.1f', k_w_vector(4)), ...
-            sprintf('k = %0.1f', k_w_vector(5)), ...
-            sprintf('k = %0.1f', k_w_vector(6)), ...
+        legend('pedida', sprintf('k = %0.1f', k_w_vector(1)), ...
+            sprintf('k_w = %0.1f', k_w_vector(2)), ...
+            sprintf('k_w = %0.1f', k_w_vector(3)), ...
+            sprintf('k_w = %0.1f', k_w_vector(4)), ...
+            sprintf('k_w = %0.1f', k_w_vector(5)), ...
+            sprintf('k_w = %0.1f', k_w_vector(6)), ...
             'location', 'west');
         xlabel('Tempo [s]');
         ylabel('Altitude h [m]');
+        saveas(fig1, '../imgs/Altitude_Sim', 'png');
 
     case 2
         % Horizontal Line Simulation
@@ -95,14 +100,15 @@ switch choice
         sim_line_results = sim('ARDroneTTSim_2019');
 
         % Plot altitude tracking for different k_w
-        figure();
+        fig2 = figure();
         hold on;
         plot(sim_line_results.pd.signals.values(1, :), sim_line_results.pd.signals.values(2, :));
         plot(sim_line_results.x_sim.signals.values, sim_line_results.y_sim.signals.values);
         % xlim([0 30]);
-        legend('desired', 'simulated', 'location', 'west');
+        legend('pedida', 'simulada', 'location', 'northwest');
         xlabel('Posição x [m]');
         ylabel('Posição y [m]');
+        saveas(fig2, '../imgs/Horizontal_line_Sim', 'png');
         
     otherwise
         %Loading Simulink model of ARDrone
