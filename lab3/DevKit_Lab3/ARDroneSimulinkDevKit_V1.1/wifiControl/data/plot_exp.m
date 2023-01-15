5d3d1892c1e30d4490ed075ee6f1410f30a77473
JoseMedeiro <56878235+JoseMedeiro@users.noreply.github.com>
2023-01-15 17:39:49 +0000

    zcz

--- lab3/DevKit_Lab3/ARDroneSimulinkDevKit_V1.1/wifiControl/data/plot_exp.m
+++ lab3/DevKit_Lab3/ARDroneSimulinkDevKit_V1.1/wifiControl/data/plot_exp.m
@@ -159,6 +159,41 @@ saveas(fig4a, '../../imgs/circ_yawcte_EXP_pitch', 'png');
 Alt_exp.desired = load('desired_data_4.mat');
 Alt_exp.results = load('exp_data_4.mat');
 
+% WHEN COMMANDS WERE ENABLED
+[~,Enabled] 	= max(logical(Alt_exp.desired.ans(8,:)'),[],1);
+[~,Active]      = max(logical(Alt_exp.desired.ans(5,:)'),[],1);
+
+% ANGLES AFTER ACTIVE
+TIME    = Alt_exp.desired.ans(1,Active:end);
+PSI.DES = Alt_exp.desired.ans(5,Active:end);
+PSI.EXP = Alt_exp.results.ans(4,Active:end);
+PHI.DES = Alt_exp.desired.ans(6,Active:end);
+PHI.EXP = Alt_exp.results.ans(2,Active:end);
+THE.DES = Alt_exp.desired.ans(7,Active:end);
+THE.EXP = Alt_exp.results.ans(3,Active:end);
+% DEMODDING OF PSI
+counter = 0;
+for i=2:length(PSI.EXP)
+    if PSI.EXP(i)+counter - PSI.EXP(i-1) > pi
+        counter = counter - 2*pi;
+    end
+    if PSI.EXP(i)+counter - PSI.EXP(i-1) < -pi
+        counter = counter + 2*pi;
+    end
+    PSI.EXP(i) = PSI.EXP(i) + counter;
+end
+counter = 0;
+for i=2:length(PSI.DES)
+    if PSI.DES(i)+counter - PSI.DES(i-1) > pi
+        counter = counter - 2*pi;
+    end
+    if PSI.DES(i)+counter - PSI.DES(i-1) < -pi
+        counter = counter + 2*pi;
+    end
+    PSI.DES(i) = PSI.DES(i) + counter;
+end
+
+% PLOT POSISAO
 fig5 = figure();
 hold on;
 plot(Alt_exp.desired.ans(2,:), Alt_exp.desired.ans(3,:));
@@ -170,36 +205,63 @@ ylim([-1 1]);
 xlim([-2 0.5]);
 saveas(fig5, '../../imgs/circ_yawvar_EXP', 'png');
 
-psi_d = mod(Alt_exp.desired.ans(5,:), 2*pi) ;
-for i=1:size(psi_d,2)
-    if psi_d(i) > pi
-        psi_d(i) = -2*pi + psi_d(i) ; 
-    end
-end
-
+% PLOT ANGLES
 fig8 = figure();
 hold on;
-plot(Alt_exp.desired.ans(1,:), psi_d);
-plot(Alt_exp.results.ans(1,:), Alt_exp.results.ans(4,:));
+plot(TIME,(PSI.EXP - PSI.DES)*180/pi);
+xlim([TIME(Active) 50]);
+legend('Erro', 'location', 'northeast');
+xlabel('Tempo [s]');
+ylabel('\psi [rad]');
+fig8 = figure();
+hold on;
+plot(TIME,THE.DES*180/pi);
+plot(TIME,THE.EXP*180/pi);
+xlim([TIME(Active) 50]);
 legend('Pedida', 'Experimental', 'location', 'northeast');
 xlabel('Tempo [s]');
-ylabel('\Psi [rad]');
-xlim([17.22 55]);
-saveas(fig8, '../../imgs/yawvar_EXP', 'png');
+ylabel('\theta [rad]');
+fig8 = figure();
+hold on;
+plot(TIME, PHI.DES*180/pi);
+plot(TIME, PHI.EXP*180/pi);
+xlim([TIME(Active) 50]);
+legend('Pedida', 'Experimental', 'location', 'northeast');
+xlabel('Tempo [s]');
+ylabel('\phi [rad]');
+
+%saveas(fig8, '../../imgs/yawvar_EXP', 'png');
 
 %% LOS - first try
 Alt_exp.desired = load('desired_data_5.mat');
 Alt_exp.results = load('exp_data_5.mat');
 
+% WHEN COMMANDS WERE ENABLED
+[~,Enabled] 	= max(logical(Alt_exp.desired.ans(8,:)'),[],1);
+[~,Active]      = max(logical(Alt_exp.desired.ans(5,:)'),[],1);
+% DISTANCE SINCE ENABLED
+x_0	= Alt_exp.results.ans(8,Enabled);
+y_0	= Alt_exp.results.ans(9,Enabled);
+% PSI AFTER ENABLED
+QUIVER_D = downsample(Alt_exp.desired.ans(:,Active:end)',30);
+QUIVER_D = QUIVER_D';
+QUIVER_R = downsample(Alt_exp.results.ans(:,Active:end)',30);
+QUIVER_R = QUIVER_R';
+
 fig6 = figure();
 hold on;
-plot(Alt_exp.desired.ans(2,:), ones(size(Alt_exp.desired.ans(2,:))));
+plot([-0.4, 1.4],[y_0 + 1, y_0 + 1]);
 plot(Alt_exp.results.ans(8,:), Alt_exp.results.ans(9,:));
-legend('Pedida', 'Experimental', 'location', 'southeast');
+plot(x_0,y_0,'*r');
+quiver(QUIVER_R(8,:), QUIVER_R(9,:),...
+       cos(QUIVER_R(4,:)), sin(QUIVER_R(4,:)),0.5);
+legend('Pedida', 'Experimental',...
+       'Inicial','\psi',...
+       'location', 'southeast');
 xlabel('Posição x [m]');
 ylabel('Posição y [m]');
-% ylim([-1 1]);
-% xlim([-2 0.5]);
+ylim([-0.2 1.4]);
+xlim([-0.4 1]);
 saveas(fig6, '../../imgs/LOS1_EXP', 'png');
 
 psi_d = mod(Alt_exp.desired.ans(5,:), 2*pi) ;
